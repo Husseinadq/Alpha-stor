bc2fb93612a032151b5dfc76e8b8070fa65270bb
husseinadq <husseinadq2@gmail.com>
2023-03-31 15:04:46 +0300

    add provider

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,15 +1,17 @@
+import 'package:alpha_stor/providers/products.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const String routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
-    final productId= ModalRoute.of(context)!.settings.arguments;
+    final productId = ModalRoute.of(context)!.settings.arguments as String;
+    final product = Provider.of<Products>(context,listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text("title"),
+        title: Text(product.title),
       ),
     );
   }
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/product_item.dart';
+import '../providers/products.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+    return GridView.builder(
+        padding: const EdgeInsets.all(10.0),
+        gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+        itemBuilder: (ctx, i) => ProductItem(
+              id: products[i].id,
+              title: products[i].title,
+              imageUrl: products[i].imageUrl,
+            ),
+        itemCount: products.length);
+  }
+}
