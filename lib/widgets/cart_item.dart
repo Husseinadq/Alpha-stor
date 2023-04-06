dac814391830f399ab9f04d054368caba94dcdfe
husseinadq <husseinadq2@gmail.com>
2023-04-06 10:56:58 +0300

    adding snakbar and dialog to cart

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -32,6 +32,27 @@ class CartItem extends StatelessWidget {
         padding: EdgeInsets.only(right: 20),
         margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
       ),
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text('Are you sure?'),
+                  content:
+                      Text('Do you want to remove the item from the cart?'),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(ctx).pop(false);
+                        },
+                        child: Text('No')),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(ctx).pop(true);
+                        },
+                        child: Text('Yes'))
+                  ],
+                ));
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -46,6 +46,17 @@ class ProductItem extends StatelessWidget {
               color: Theme.of(context).colorScheme.secondary,
               onPressed: () {
                 cart.addItem(product.id, product.price, product.title);
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('Added item to cart!'),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: (() {
+                      cart.removeSingleItem(product.id);
+                    }),
+                  ),
+                ));
               },
             ),
           )),
